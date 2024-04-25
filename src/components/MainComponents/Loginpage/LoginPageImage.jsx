58380e1d9f3a003cbbd60e7ca5755d53e47e7a74
Valentine Omonya <valomosh254@gmail.com>
2024-04-26 00:24:10 +0300

    Added login page

--- src/components/MainComponents/Loginpage/LoginPageImage.jsx
+++ src/components/MainComponents/Loginpage/LoginPageImage.jsx
@@ -10,10 +10,30 @@ const LoginPageImage = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className="h-56 w-96 bg-white border rounded-md flex justify-center items-center">
-        <h1 className="font-semibold text-2xl text-black">
-          Login with <span className="text-gray-500  underline">Google</span> or get banned for life.
-        </h1>
+      <div className="h-56 w-96 border-blue-700 bg-white border-2 rounded-md flex justify-center items-center">
+        <div className="flex flex-row w-full">
+          <div className="flex justify-between w-full flex-col px-1">
+            <div className="flex justify-between items-center w-full mb-4">
+              <span className="text-md font-bold italic">Google Firebase</span>
+              <span className="text-sm font-thin">30 days login Session</span>
+            </div>
+            <div className="text-md italic font-semibold text-start">
+              <p>
+                Login with google to access this todo list app this is to allow
+                for saving your todo's in our application which works with
+                Firebase
+              </p>
+            </div>
+          </div>
+          <div className="w-28 flex justify-center  items-center flex-col">
+            <div>
+                <h2 className="text-md font-bold italic">22930</h2>
+            </div>
+            <div className="mt-6 text-sky-500">
+                View More
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
